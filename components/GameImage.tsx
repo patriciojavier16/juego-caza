395db769838ea3b32a866977fffedf23c085bc0f
Jacob <jacobowiz.7@protonmail.com>
2023-12-06 18:01:51 -0500

    First change list Images

---
+++ components/GameImage.tsx
@@ -0,0 +1,23 @@
+import { View, Text, Image, StyleSheet } from 'react-native'
+import React from 'react'
+
+export default function GameImage(props: any) {
+  return (
+    <View>
+        <View style={{flexDirection: 'row-reverse'}}>
+            <Text>{props.data.nombre}</Text>
+            <Image
+                source={{ uri: props.data.url }}
+                style={styles.img1}
+            />
+        </View>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+    img1: {
+        with: 200,
+        height: 200
+    }
+});
--- screens/GameScreen.tsx
+++ screens/GameScreen.tsx
@@ -1,12 +1,33 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, FlatList } from 'react-native'
 import React from 'react'
+import GameImage from '../components/GameImage'
 
 export default function GameScreen() {
+  const imagenes = [
+    { nombre: 'Image1', url: 'https://proyectosbeta.net/wp-content/uploads/2013/04/WallPapers_Insectos01.jpg'},
+    { nombre: 'Image2', url: 'https://blog.foto24.com/wp-content/uploads/2014/03/cabecera-672x372-fotos-insectos-inspiracion.jpg'},
+  ]
   return (
-    <View>
-      <Text>GameScreen</Text>
+    <View style={styles.container}>
+      <Text style={{fontSize: 40, color: 'blue'}}>Galeria</Text>
+      <View style={{ height: 500, backgroundColor: 'blue'}}>
+        <FlatList
+          data={imagenes}
+          horizontal={false}
+          renderItem={({ item }) => (
+            <GameImage data={item} />
+          )}
+        />
+      </View>
     </View>
   )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#2c3e79'
+  }
+})
