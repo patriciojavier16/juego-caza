5cc62f7e7a6fe9058c7060ef7726fca631c30c6f
patriciojavier16 <patriciomoralespaucar@gmail.com>
2023-12-06 10:21:16 -0500

    commit-navegator

---
+++ navegators/Navigator.tsx
@@ -0,0 +1,35 @@
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createStackNavigator } from "@react-navigation/stack";
+import { NavigationContainer } from '@react-navigation/native';
+import LoginScreen from '../screens/LoginScreen';
+import GameScreen from '../screens/GameScreen';
+import ScoreScreen from '../screens/ScoreScreen';
+
+const Tab = createBottomTabNavigator();
+const Stack = createStackNavigator();
+
+
+function MyTabs() {
+  return (
+    <Tab.Navigator initialRouteName='Lista'>
+      <Tab.Screen name="Game" component={ GameScreen } />
+      <Tab.Screen name="Score" component={ ScoreScreen } />
+    </Tab.Navigator>
+  );
+}
+function MyStack() {
+  return (
+      <Stack.Navigator initialRouteName="Login">
+          <Stack.Screen name="Login" component={LoginScreen} />
+          <Stack.Screen name="HOME" component={MyTabs} />
+      </Stack.Navigator>
+  )
+}
+
+export default function BottomNavigator (){
+    return(
+        <NavigationContainer>
+            <MyStack/>
+        </NavigationContainer>
+    )
+}
